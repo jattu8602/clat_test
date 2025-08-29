f747c496e524496bb356d52353e8e641dfca1272
jattu8602 <chaurasiyajatin68@gmail.com>
2025-08-30 00:33:27 +0530

    Enhance Dialog component with additional spacing
    - Added extra spacing in the DialogOverlay component for improved layout and visual clarity.

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -23,6 +23,8 @@ const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
     {...props}
   />
 ))
+
+
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef(
